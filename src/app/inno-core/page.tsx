1f75b25d9c506febb029784ed4f0cdbdc8eec062
SamedKizilhan <skizilhan35@gmail.com>
2025-07-02 23:04:51 +0300

    InnoCore sayfasında ürün bilgileri geçici olarak kapatıldı.

--- src/app/inno-core/page.tsx
+++ src/app/inno-core/page.tsx
@@ -182,7 +182,7 @@ const InnoCorePage = () => {
                     </div>
 
                     {/* Products Grid */}
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                    {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                         {products.map((product) => (
                             <InnoCoreCard
                                 key={product.id}
@@ -194,7 +194,7 @@ const InnoCorePage = () => {
                                 category={t(product.category)}
                             />
                         ))}
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
